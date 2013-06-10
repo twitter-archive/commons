00c9fc15be786928227f5ccf6d152a9a55726729
Brian Wickman <wickman@twitter.com>
2013-06-10 14:55:18 -0700

    [PATCH 025/153] add lifecycle commands to RootServer module
    RB_ID=153356
    NO-QUEUE=true
    
    (sapling split of 951eb1ca400b5ad98f0fe7c055693e7c915eee57)

--- src/python/twitter/common/app/modules/http.py
+++ src/python/twitter/common/app/modules/http.py
@@ -15,9 +15,21 @@
 # ==================================================================================================
 
 import threading
-from twitter.common.http.server import HttpServer
-from twitter.common.http.diagnostics import DiagnosticsEndpoints
+
 from twitter.common import app, options
+from twitter.common.http.diagnostics import DiagnosticsEndpoints
+from twitter.common.http.server import HttpServer
+
+
+class LifecycleEndpoints(object):
+  @HttpServer.route('/quitquitquit', method='POST')
+  def quitquitquit(self):
+    app.quitquitquit()
+
+  @HttpServer.route('/abortabortabort', method='POST')
+  def abortabortabort(self):
+    app.abortabortabort()
+
 
 class RootServer(HttpServer, app.Module):
   """
@@ -31,6 +43,13 @@ class RootServer(HttpServer, app.Module):
           action='store_true',
           dest='twitter_common_http_root_server_enabled',
           help='Enable root http server for various subsystems, e.g. metrics exporting.'),
+
+    'disable_lifecycle':
+      options.Option('--http_disable_lifecycle',
+          default=False,
+          action='store_true',
+          dest='twitter_common_http_root_server_disable_lifecycle',
+          help='Disable the lifecycle commands, i.e. /quitquitquit and /abortabortabort.'),
 
     'port':
       options.Option('--http_port',
@@ -68,6 +87,8 @@ class RootServer(HttpServer, app.Module):
     parent = self
 
     self.mount_routes(DiagnosticsEndpoints())
+    if not options.twitter_common_http_root_server_disable_lifecycle:
+      self.mount_routes(LifecycleEndpoints())
 
     class RootServerThread(threading.Thread):
       def __init__(self):
