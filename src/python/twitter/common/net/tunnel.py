a079ad6627fb82d0229735ccc47c11beed1af402
Kevin Sweeney <ksweeney@twitter.com>
2013-09-16 20:36:54 +0000

    [PATCH 080/153] Don't start a remote shell when creating an ssh tunnel
    RB_ID=187755
    
    (sapling split of a7afb7c3d9bd662619269890686309323de7f396)

--- src/python/twitter/common/net/tunnel.py
+++ src/python/twitter/common/net/tunnel.py
@@ -126,7 +126,7 @@ class TunnelHelper(object):
     tunnel_port = tunnel_port or cls.get_random_port()
     cls.log('opening connection to %s:%s via %s:%s' %
         (remote_host, remote_port, tunnel_host, tunnel_port))
-    ssh_cmd_args = ('ssh', '-T', '-L', '%d:%s:%s' % (tunnel_port, remote_host, remote_port),
+    ssh_cmd_args = ('ssh', '-N', '-T', '-L', '%d:%s:%s' % (tunnel_port, remote_host, remote_port),
                     tunnel_host)
     ssh_popen = subprocess.Popen(ssh_cmd_args, stdin=subprocess.PIPE)
     cls.TUNNELS[tunnel_key] = tunnel_port, ssh_popen
@@ -144,7 +144,7 @@ class TunnelHelper(object):
       return 'localhost', cls.PROXIES[proxy_host][0]
     proxy_host, proxy_port = cls.acquire_host_pair(proxy_host, proxy_port)
     cls.log('opening SOCKS proxy connection through %s:%s' % (proxy_host, proxy_port))
-    ssh_cmd_args = ('ssh', '-T', '-D', str(proxy_port), proxy_host)
+    ssh_cmd_args = ('ssh', '-N', '-T', '-D', str(proxy_port), proxy_host)
     ssh_popen = subprocess.Popen(ssh_cmd_args, stdin=subprocess.PIPE)
     cls.PROXIES[proxy_host] = (proxy_port, ssh_popen)
     if not cls.wait_for_accept(proxy_port, ssh_popen, timeout):
