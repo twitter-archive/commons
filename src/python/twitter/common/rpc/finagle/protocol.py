eee47c2b3ccacc42504157a75d978887e810c722
Brian Wickman <wickman@twitter.com>
2013-11-18 18:00:04 +0000

    [PATCH 123/153] finish twitter.common artifact publishes
     - fix rpc, add __init__ and apache boilerplate
     - create concurrent, rpc, net, http, recordio splits for pypi
     - split common.net and common_internal.net
    
    RB_ID=233609
    
    (sapling split of 75d08f951cbe60b31d8670549e14beab70c801e6)

--- src/python/twitter/common/rpc/finagle/protocol.py
+++ src/python/twitter/common/rpc/finagle/protocol.py
@@ -1,17 +1,35 @@
+# ==================================================================================================
+# Copyright 2013 Twitter, Inc.
+# --------------------------------------------------------------------------------------------------
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this work except in compliance with the License.
+# You may obtain a copy of the License in the LICENSE file, or at:
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==================================================================================================
+
 import functools
 import threading
-from thrift.Thrift import (
-  TMessageType,
-  TApplicationException)
-from thrift.protocol import TBinaryProtocol
-from gen.twitter.finagle.thrift.ttypes import (
-  ConnectionOptions,
-  ClientId,
-  UpgradeReply,
-  RequestHeader,
-  ResponseHeader)
 
-from twitter.common.rpc.finagle.trace import Trace
+from thrift.Thrift import TApplicationException, TMessageType
+from thrift.protocol import TBinaryProtocol
+
+from gen.twitter.finagle.thrift.ttypes import (
+    ClientId,
+    ConnectionOptions,
+    RequestHeader,
+    ResponseHeader,
+    UpgradeReply,
+)
+
+from .trace import Trace
+
 
 def upgrade_protocol_to_finagle(protocol):
   UPGRADE_METHOD = "__can__finagle__trace__v3__"
@@ -39,6 +57,7 @@ def upgrade_protocol_to_finagle(protocol):
 
   send(protocol)
   return recv(protocol)
+
 
 class TFinagleProtocol(TBinaryProtocol.TBinaryProtocolAccelerated):
   def __init__(self, *args, **kw):
