6ea2044a098054983e64a79b57ccbd3157f6509b
Joshua Cohen <jcohen@twitter.com>
2014-01-16 22:46:23 +0000

    [PATCH 3837/7508] Add a ContainerResponseFilter that allows for tracking response stats on a per-request category basis.
    RB_ID=263351
    
    (sapling split of 0554ce56dffd837905dbbe01248dacaf89fc3fae)

--- tests/java/com/twitter/common/net/http/filters/HttpStatsFilterTest.java
+++ tests/java/com/twitter/common/net/http/filters/HttpStatsFilterTest.java
@@ -1,22 +1,33 @@
 package com.twitter.common.net.http.filters;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
 
-import org.easymock.EasyMock;
-import org.easymock.IAnswer;
+import com.google.common.collect.Lists;
+import com.sun.jersey.api.core.ExtendedUriInfo;
+import com.sun.jersey.api.model.AbstractResourceMethod;
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.twitter.common.collections.Pair;
+import com.twitter.common.net.http.filters.HttpStatsFilter.TrackRequestStats;
 import com.twitter.common.quantity.Amount;
 import com.twitter.common.quantity.Time;
 import com.twitter.common.stats.SlidingStats;
 import com.twitter.common.testing.easymock.EasyMockTest;
 import com.twitter.common.util.testing.FakeClock;
 
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.expectLastCall;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -27,6 +38,30 @@ public class HttpStatsFilterTest extends EasyMockTest {
   private HttpServletResponse response;
   private FilterChain filterChain;
   private HttpStatsFilter filter;
+  private ContainerRequest containerRequest;
+  private ContainerResponse containerResponse;
+  private ExtendedUriInfo extendedUriInfo;
+  private HttpServletRequest servletRequest;
+
+  private static final Amount<Long, Time> REQUEST_TIME =  Amount.of(1000L, Time.NANOSECONDS);
+
+  private void injectContextVars() throws Exception {
+    extendedUriInfo = createMock(ExtendedUriInfo.class);
+    servletRequest = createMock(HttpServletRequest.class);
+
+    List<Object> injectables = Lists.newArrayList(extendedUriInfo, servletRequest);
+
+    for (Field f : filter.getClass().getDeclaredFields()) {
+      if (f.isAnnotationPresent(Context.class)) {
+        for (Object injectable : injectables) {
+          if (f.getType().isInstance(injectable)) {
+            f.setAccessible(true);
+            f.set(filter, injectable);
+          }
+        }
+      }
+    }
+  }
 
   @Before
   public void setUp() throws Exception {
@@ -35,57 +70,41 @@ public class HttpStatsFilterTest extends EasyMockTest {
     response = createMock(HttpServletResponse.class);
     filterChain = createMock(FilterChain.class);
     filter = new HttpStatsFilter(clock);
+
+    containerRequest = createMock(ContainerRequest.class);
+    containerResponse = createMock(ContainerResponse.class);
+
+    injectContextVars();
   }
 
   @Test
-  public void testStatGathering() throws Exception {
-    final Amount<Long, Time> responseTime = Amount.of(100L, Time.NANOSECONDS);
-    int numCalls = 2;
-
-    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    expectLastCall().times(numCalls);
-
-    filterChain.doFilter(
-        EasyMock.anyObject(HttpServletRequest.class),
-        EasyMock.anyObject(HttpServletResponse.class));
-    expectLastCall().andAnswer(new IAnswer<Void>() {
-      @Override
-      public Void answer() throws Throwable {
-        clock.advance(responseTime);
-        HttpServletResponse responseArg = (HttpServletResponse) EasyMock.getCurrentArguments()[1];
-        responseArg.setStatus(HttpServletResponse.SC_NOT_FOUND);
-
-        return null;
-      }
-    }).times(numCalls);
+  public void testStartTimeIsSetAsRequestAttribute() throws Exception {
+    request.setAttribute(HttpStatsFilter.REQUEST_START_TIME, REQUEST_TIME.getValue());
+    filterChain.doFilter(request, response);
 
     control.replay();
 
-    for (int i = 0; i < numCalls; i++) {
-      filter.doFilter(request, response, filterChain);
-    }
-
-    SlidingStats stat = filter.counters.get(HttpServletResponse.SC_NOT_FOUND);
-    assertEquals(responseTime.getValue() * numCalls, stat.getTotalCounter().get());
-    assertEquals(numCalls, stat.getEventCounter().get());
+    clock.advance(REQUEST_TIME);
+    filter.doFilter(request, response, filterChain);
   }
 
   @Test
   public void testExceptionStatsCounting() throws Exception {
-    filterChain.doFilter(
-        EasyMock.anyObject(HttpServletRequest.class),
-        EasyMock.anyObject(HttpServletResponse.class));
+    request.setAttribute(HttpStatsFilter.REQUEST_START_TIME, REQUEST_TIME.getValue());
+    expectLastCall().times(2);
+    clock.advance(REQUEST_TIME);
+
+    filterChain.doFilter(anyObject(HttpServletRequest.class), anyObject(HttpServletResponse.class));
     expectLastCall().andThrow(new IOException());
-    filterChain.doFilter(
-        EasyMock.anyObject(HttpServletRequest.class),
-        EasyMock.anyObject(HttpServletResponse.class));
+
+    filterChain.doFilter(anyObject(HttpServletRequest.class), anyObject(HttpServletResponse.class));
     expectLastCall().andThrow(new ServletException());
 
     control.replay();
 
     try {
       filter.doFilter(request, response, filterChain);
-      fail();
+      fail("Filter should have re-thrown the exception.");
     } catch (IOException e) {
       // Exception is expected, but we still want to assert on the stat tracking, so we can't
       //  just use @Test(expected...)
@@ -94,10 +113,85 @@ public class HttpStatsFilterTest extends EasyMockTest {
 
     try {
       filter.doFilter(request, response, filterChain);
-      fail();
+      fail("Filter should have re-thrown the exception.");
     } catch (ServletException e) {
       // See above.
       assertEquals(2, filter.exceptionCount.get());
     }
+  }
+
+  private void expectIdentifier(String identifier, int times) {
+    AbstractResourceMethod matchedMethod = createMock(AbstractResourceMethod.class);
+    expect(extendedUriInfo.getMatchedMethod()).andReturn(matchedMethod).times(times);
+
+    TrackRequestStats annotation = createMock(TrackRequestStats.class);
+    expect(matchedMethod.getAnnotation(TrackRequestStats.class)).andReturn(annotation).times(times);
+
+    expect(annotation.identifier()).andReturn(identifier).times(times);
+  }
+
+  private void expectIdentifier(String identifier) {
+    expectIdentifier(identifier, 1);
+  }
+
+  @Test
+  public void testBasicStatsCounting() throws Exception {
+    expect(containerResponse.getStatus()).andReturn(HttpServletResponse.SC_OK);
+
+    expect(servletRequest.getAttribute(HttpStatsFilter.REQUEST_START_TIME))
+        .andReturn(clock.nowNanos());
+
+    String identifier = "some_identifier";
+    expectIdentifier(identifier);
+
+    control.replay();
+
+    clock.advance(REQUEST_TIME);
+    assertEquals(containerResponse, filter.filter(containerRequest, containerResponse));
+
+    SlidingStats stat = filter.requestCounters.get(Pair.of(identifier, HttpServletResponse.SC_OK));
+    assertEquals(1, stat.getEventCounter().get());
+    assertEquals(REQUEST_TIME.getValue().longValue(), stat.getTotalCounter().get());
+    assertEquals(1, filter.statusCounters.get(HttpServletResponse.SC_OK).getEventCounter().get());
+  }
+
+  @Test
+  public void testMultipleRequests() throws Exception {
+    int numCalls = 2;
+
+    expect(containerResponse.getStatus()).andReturn(HttpServletResponse.SC_OK).times(numCalls);
+
+    expect(servletRequest.getAttribute(HttpStatsFilter.REQUEST_START_TIME))
+        .andReturn(clock.nowNanos()).times(numCalls);
+
+    String identifier = "some_identifier";
+    expectIdentifier(identifier, numCalls);
+
+    control.replay();
+
+    clock.advance(REQUEST_TIME);
+    for (int i = 0; i < numCalls; i++) {
+      filter.filter(containerRequest, containerResponse);
+    }
+
+    SlidingStats stat = filter.requestCounters.get(Pair.of(identifier, HttpServletResponse.SC_OK));
+    assertEquals(numCalls, stat.getEventCounter().get());
+    assertEquals(REQUEST_TIME.getValue() * numCalls, stat.getTotalCounter().get());
+    assertEquals(numCalls,
+        filter.statusCounters.get(HttpServletResponse.SC_OK).getEventCounter().get());
+  }
+
+  @Test
+  public void testNoStartTime() throws Exception {
+    expect(servletRequest.getAttribute(HttpStatsFilter.REQUEST_START_TIME))
+        .andReturn(null);
+
+    expect(containerResponse.getStatus()).andReturn(HttpServletResponse.SC_OK);
+
+    control.replay();
+
+    assertEquals(containerResponse, filter.filter(containerRequest, containerResponse));
+
+    assertEquals(0, filter.statusCounters.asMap().keySet().size());
   }
 }
