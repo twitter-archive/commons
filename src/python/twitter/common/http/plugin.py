87435a5e57139b2427134c5208f0ded2a2c82249
Kris Wilson <kwilson@twitter.com>
2014-02-18 22:15:37 +0000

    [PATCH 15/16] Implement twitter.common.http kerberos plugin + examples
    RB_ID=284529
    
    (sapling split of 59ccbb77447b03ac91048aced8769acf491b9517)

--- src/python/twitter/common/http/plugin.py
+++ src/python/twitter/common/http/plugin.py
@@ -1,13 +1,6 @@
-from __future__ import print_function
-
 from abc import abstractmethod
-from functools import wraps
-import pprint
-import time
 
 from twitter.common.lang import Interface
-
-from .server import request
 
 
 class Plugin(Interface):
@@ -37,21 +30,3 @@ class Plugin(Interface):
   def close(self):
     pass
 
-
-class EchoHeaders(Plugin):
-  """An example Plugin that prints to stdout request information as it comes in."""
-  def apply(self, callback, route):
-    @wraps(callback)
-    def wrapped_callback(*args, **kw):
-      print('path: %s' % request.path)
-      print('method: %s' % request.method)
-      print('cookies: ', end='')
-      pprint.pprint(dict(request.cookies.items()))
-      print('query: ', end='')
-      pprint.pprint(dict(request.query.items()))
-      print('params: ', end='')
-      pprint.pprint(dict(request.params.items()))
-      print('headers: ', end='')
-      pprint.pprint(dict(request.headers.items()))
-      return callback(*args, **kw)
-    return wrapped_callback
