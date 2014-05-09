d83d8eb13ee3f87774b5b6cb2b5ec4ef60abfc6c
David Turner <dturner@twitter.com>
2014-05-09 16:15:12 -0700

    Add history migration script.

---
+++ src/bash/git/migrate-history/migrate-history.sh
@@ -0,0 +1,168 @@
+#!/usr/bin/env bash
+
+# This is a script to migrate a git repository to a subdirectory of
+# another, while preserving history.
+
+# I would like to use git subtree, but git subtree doesn't rewrite
+# imported history, so files appear to move.  That means that git log
+# on the subdirectory won't work.  You have to do git log --follow to
+# see the history of a file, and you can only do that on an individual
+# file basis.
+
+# Also, I'm not sure what git subtree does with non-linear history.
+# Git rebase fails on fake conflicts as it attempts cross the streams
+# (that is, rewrite history as if it were all one branch).  And many
+# projects have non-linear history on master.
+
+# So instead I use the git plumbing commands to build a new commit for
+# every commit on the imported tree.  The new commit contains the
+# original commit's tree within its tree object.  So, it's basically a
+# multibranch, cross-repo rebase.
+
+# This stuff is for bash 3 compatibility; bash 4 has associative
+# arrays.
+map_insert () {
+    local map_name=$1 key=$2 val=$3
+    eval __map_${map_name}_${key}=$val
+}
+
+map_find () {
+    local map_name=$1 key=$2
+    local var=__map_${map_name}_${key}
+    echo ${!var}
+}
+
+die () {
+    test -n $subtree_remote_name && git remote rm "$subtree_remote_name"
+    echo "$*"
+    exit 1
+}
+
+make_subtreed_tree () {
+    subtree_tree="$1"
+    parent_tree="$2"
+    subtree_dir="$3" || die "missing subtree directory"
+    tree_contents=$(git ls-tree "$parent_tree") || die "ls-tree"
+    new_tree_line=$(echo -e "\n040000 tree $subtree_tree\t$subtree_dir")
+    tree_contents+="$new_tree_line"
+    tree_contents=$(echo "$tree_contents" | sort -k 4) || die "sort"
+    echo "$tree_contents" | git mktree || die "mktree"
+}
+
+usage() {
+    cat <<EOF
+Usage: migrate-history.sh [-b branch] [-s subdir] <path-to-immigrant>"
+
+Migrate the repository at <path-to-repo> into this repository at
+subdir.  If -s is omitted, it will be the basename of
+<path-to-repo>.
+
+You can use -b to get a branch other than master from the immigrant.
+This repository's master will always be used.
+
+Run this script from inside your repository, at the root directory.  The
+result will be a branch named migrated-\$subdir
+
+EOF
+
+    exit 1
+}
+
+log() {
+    echo "$@" 1>&2
+}
+
+#check for clean index
+git diff-index --quiet --cached HEAD || die "This command should be run on a clean index."
+#and clean working tree
+git diff-files --quiet || die "This command should be run on a clean working tree"
+
+while getopts ":s:b:" OPTION
+do
+    case "$OPTION" in
+	b) branch="$OPTARG" ;;
+	s) subdir="$OPTARG" ;;
+	--) shift ; break ;;
+	*) usage
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+test "$#" = 1 || usage
+
+path_to_project="$*"
+
+#get real path to project
+path_to_project=$(
+  cd $path_to_project
+  pwd
+)
+
+[[ -n $subdir ]] || subdir=$(basename "$path_to_project")
+[[ -n $branch ]] || branch=master
+
+log "Creating a remote for the imported project so I can easily get commits from it."
+
+timestamp=$(date +"%Y%m%d%H%M%S")
+subtree_remote_name="remote-$timestamp-$$-$subdir"
+
+git remote add "$subtree_remote_name" "$path_to_project" || die "Can't add remote $subtree_remote_name for $path_to_project"
+
+log "Fetching from imported project"
+git fetch "$subtree_remote_name"
+
+localbranch="migrate-$timestamp-$$"
+
+log "Creating a branch for the migration"
+git checkout -q -b "$localbranch" "$subtree_remote_name/$branch" || die "Can't branch"
+
+commits=$(git rev-list --reverse --topo-order "$localbranch")
+
+count=0
+total=$(echo "$commits" | wc -l)
+
+log "Migrating commits"
+for commit in $commits
+do
+    parents=$(git log --pretty="%P" $commit -n 1) || die "Expected $commit to exist"
+
+    rebased_parents=""
+    for orig_parent in $parents
+    do
+	rebased_parent_commit=$(map_find commit_map $orig_parent)
+	[[ -n $rebased_parent_commit ]] || die "I lost track of $orig_parent"
+        rebased_parents="$rebased_parents -p $rebased_parent_commit"
+    done
+    #if there are no parents, this is the first commit; assume the parent
+    #is this repo's master
+    [[ -n $rebased_parents ]] || rebased_parents="-p master"
+
+    #we want to create a new tree that represents the contents of
+    #this commit as-rebased.
+
+    commit_tree=$(git rev-parse $commit^{tree})
+    new_tree=$(make_subtreed_tree $commit_tree master $subdir)
+    merge_commit_message=$(git log --format=%B -n 1 $commit)
+    committed=$(echo "$merge_commit_message" | \
+	GIT_AUTHOR_NAME=$(git log --format=%an -n 1 $commit) \
+	GIT_AUTHOR_EMAIL=$(git log --format=%ae -n 1 $commit) \
+	GIT_AUTHOR_DATE=$(git log --format=%aD -n 1 $commit) \
+	GIT_COMMITTER_NAME=$(git log --format=%cn -n 1 $commit) \
+	GIT_COMMITTER_EMAIL=$(git log --format=%ce -n 1 $commit) \
+	GIT_COMMITTER_DATE=$(git log --format=%aD -n 1 $commit) \
+	EMAIL=$GIT_COMMITTER_EMAIL \
+	git commit-tree $new_tree $rebased_parents)
+    map_insert commit_map $commit $committed
+    last_commit=$committed
+    count=$(expr $count + 1)
+    log "committed new revision $committed ($count/$total)"
+done
+
+log "Switching to new commit history"
+git reset -q --hard $committed
+
+log "Cleaning up remote"
+git remote rm "$subtree_remote_name"
+
+echo $committed
---
+++ src/bash/git/migrate-history/test-migrate-history.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+
+# This is a test script for migrate-history.sh.  It sets up a couple
+# of test repositories, then runs migrate-history.sh and checks that
+# the results are as-expected.
+
+die () {
+    echo "$*"
+    exit 1
+}
+
+
+#Test script for test.sh
+
+[[ -e immigrant ]] && die "Directory named immigrant already exists.  Please remove it."
+[[ -e recpient ]] && die "Directory named recipient already exists. Please remove it."
+
+#Create the immigrant repository
+(
+mkdir immigrant
+cd immigrant
+git init
+echo "This is the immigrant repo" > README
+git add README
+git commit README -m 'commit 1'
+
+#Now let's create some branching history
+git checkout -b branch-b
+git checkout master
+echo "Branch the first" > README
+git add README
+git commit README -m 'branch a'
+git checkout master
+
+#Contents of branch b
+git checkout branch-b
+echo "Branch the second" > README
+git add README
+git commit README -m 'branch b'
+git checkout master
+
+git merge branch-b #there are conflicts
+echo "Merged" > README
+git add README
+git commit -m 'merged'
+)
+immigrant_revisions=$(
+    cd immigrant
+    git rev-list HEAD|wc -l
+)
+
+branch_b_revisions=$(
+    cd immigrant
+    git rev-list branch-b|wc -l
+)
+
+#Create the recipient repository
+mkdir recipient
+cd recipient
+git init
+echo -n "I am a recipient repo" > README
+mkdir dir
+echo "I am in a subdir" > dir/README
+git add README dir/README
+git commit -m 'I am a commit on recipient'
+mkdir post
+echo "I am in a different subdir" > post/README
+git add post/README
+git commit -m 'I am another commit on recipient'
+master_revisions=$(git rev-list HEAD|wc -l)
+clean_recipient=$(git rev-parse HEAD)
+
+#migrate in the repo
+
+#Tests
+echo "Testing migrate-history.sh with no options"
+
+output=$(../migrate-history.sh ../immigrant) || die "Failed to migrate"
+[[ $output == $(git rev-parse HEAD) ]] || die "Output consists of something other than sha of new HEAD $output"
+
+revisions=$(git rev-list HEAD|wc -l)
+expected_revisions=$(expr "$master_revisions" + "$immigrant_revisions")
+[[ $revisions == $expected_revisions ]] || die "Wrong number of revisions"
+[[ "I am a recipient repo" == $(cat README) ]] || die "We overwrote README in recipient"
+[[ -e dir/README ]] || die "We deleted dir in recipient"
+[[ -e post/README ]] || die "We deleted post in recipient"
+
+[[ "Merged" == $(cat immigrant/README) ]] || die "Wrong content imported for immigrant/README"
+migrated_revisions=$(git log --format=oneline immigrant | wc -l)
+[[ $migrated_revisions == $immigrant_revisions ]] || die "Wrong number of revisions for immigrant"
+last_immigrant_commit=$(git log --format=oneline --parents -n 1 immigrant)
+echo "$last_immigrant_commit" | egrep '[a-f0-9]{40} [a-f0-9]{40} merged' || die "commit graph not imported (bad last commit)"
+first_immigrant_commit=$(git log --format=oneline --parents immigrant | tail -1)
+echo "$first_immigrant_commit" | egrep '[a-f0-9]{40} commit 1' || die "commit graph not imported (bad first commit)"
+
+#Prepare for next test
+git reset --hard "$clean_recipient"
+
+echo "Testing migrate-history.sh with -s"
+../migrate-history.sh -s the_subdir ../immigrant || die "Failed to migrate"
+[[ "I am a recipient repo" == $(cat README) ]] || die "We overwrote README in recipient"
+[[ -e dir/README ]] || die "We deleted dir in recipient"
+[[ -e post/README ]] || die "We deleted post in recipient"
+
+[[ ! -e immigrant ]] || die "We migrated to the wrong place"
+
+[[ "Merged" == $(cat the_subdir/README) ]] || die "Wrong content imported for the_subdir/README"
+migrated_revisions=$(git log --format=oneline the_subdir | wc -l)
+[[ $migrated_revisions == $immigrant_revisions ]] || die "Wrong number of revisions for immigrant"
+last_immigrant_commit=$(git log --format=oneline --parents -n 1 the_subdir)
+echo "$last_immigrant_commit" | egrep '[a-f0-9]{40} [a-f0-9]{40} merged' || die "commit graph not imported (bad last commit)"
+
+#Prepare for next test
+git reset --hard "$clean_recipient"
+
+echo "Testing migrate-history.sh with -b"
+../migrate-history.sh -b branch-b ../immigrant || die "Failed to migrate"
+[[ "I am a recipient repo" == $(cat README) ]] || die "We overwrote README in recipient"
+[[ -e dir/README ]] || die "We deleted dir in recipient"
+[[ -e post/README ]] || die "We deleted post in recipient"
+
+[[ "Branch the second" == $(cat immigrant/README) ]] || die "Wrong content imported for immigrant/README"
+migrated_revisions=$(git log --format=oneline immigrant | wc -l)
+[[ $migrated_revisions == $branch_b_revisions ]] || die "Wrong number of revisions for immigrant"
+last_immigrant_commit=$(git log --format=oneline --parents -n 1 immigrant)
+echo "$last_immigrant_commit" | egrep '[a-f0-9]{40} branch b' || die "commit graph not imported (bad last commit)"
+
+
+#Cleanup
+cd ..
+rm -rf immigrant
+rm -rf recipient
+
+echo "All tests passed"
