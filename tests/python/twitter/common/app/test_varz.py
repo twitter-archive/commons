f40ed7959dec2c1e3a8f9fcff227de64c61c488c
Alex Yarmula <ak@twitter.com>
2014-06-17 15:16:29 -0700

    fix review feedback

--- tests/python/twitter/common/app/test_varz.py
+++ tests/python/twitter/common/app/test_varz.py
@@ -17,70 +17,69 @@
 import unittest
 
 from twitter.common.app.modules.varz import VarsEndpoint, VarsSubsystem
+from twitter.common.http.server import request
 from twitter.common.quantity import Amount, Time
 from twitter.common.metrics import NamedGauge, RootMetrics
 
-from bottle import request
 import json
 import pytest
 
 
 class TestVarz(unittest.TestCase):
   def test_breaking_out_regex(self):
-  	varsSubsystem = VarsSubsystem()
-  	regex = varsSubsystem.parse_stats_filters("alpha,beta.*")
-  	assert(regex.match("alpha"))
-  	assert(not regex.match("something_alpha_something"))
-  	assert(regex.match("beta"))
-  	assert(regex.match("beta_suffix"))
-  	assert(not regex.match("abeta"))
+    vars_subsystem = VarsSubsystem()
+    regex = vars_subsystem.compile_stats_filters(["alpha", "beta.*"])
+    assert regex.match("alpha")
+    assert not regex.match("something_alpha_something")
+    assert regex.match("beta")
+    assert regex.match("beta_suffix")
+    assert not regex.match("abeta")
 
   def test_filtering_vars_filter_enabled_and_requested(self):
-  	rm = RootMetrics()
-  	zone = NamedGauge('alpha', "wont_be_visible")
-  	alpha = NamedGauge('zone', "smf1")
-  	rm.register(zone)
-  	rm.register(alpha)
-
-	metrics = RootMetrics()
-  	varsSubsystem = VarsSubsystem()
-  	regex = varsSubsystem.parse_stats_filters("alpha,beta.*")
-  	endpoint = VarsEndpoint(period = Amount(60000, Time.MILLISECONDS), stats_filter = regex)
-  	request.GET.append('filtered', '1')
-  	metrics_returned = endpoint.handle_vars_json()
-  	assert("zone" in metrics_returned)
-  	assert("alpha" not in metrics_returned)
-  	request.GET.replace('filtered', None)
+    rm = RootMetrics()
+    zone = NamedGauge('alpha', "wont_be_visible")
+    alpha = NamedGauge('zone', "smf1")
+    rm.register(zone)
+    rm.register(alpha)
+    metrics = RootMetrics()
+    vars_subsystem = VarsSubsystem()
+    regex = vars_subsystem.compile_stats_filters(["alpha", "beta.*"])
+    endpoint = VarsEndpoint(period=Amount(60000, Time.MILLISECONDS), stats_filter=regex)
+    request.GET.append('filtered', '1')
+    metrics_returned = endpoint.handle_vars_json()
+    assert "zone" in metrics_returned
+    assert "alpha" not in metrics_returned
+    request.GET.replace('filtered', None)
 
   def test_filtering_vars_filter_enabled_and_not_requested(self):
-  	rm = RootMetrics()
-  	zone = NamedGauge('alpha', "wont_be_visible")
-  	alpha = NamedGauge('zone', "smf1")
-  	rm.register(zone)
-  	rm.register(alpha)
+    rm = RootMetrics()
+    zone = NamedGauge('alpha', "wont_be_visible")
+    alpha = NamedGauge('zone', "smf1")
+    rm.register(zone)
+    rm.register(alpha)
 
-	metrics = RootMetrics()
-  	varsSubsystem = VarsSubsystem()
-  	regex = varsSubsystem.parse_stats_filters("alpha,beta.*")
-  	endpoint = VarsEndpoint(period = Amount(60000, Time.MILLISECONDS), stats_filter = regex)
-  	metrics_returned = endpoint.handle_vars_json()
-  	assert("zone" in metrics_returned)
-  	assert("alpha" in metrics_returned)
-	request.GET.replace('filtered', None)
+    metrics = RootMetrics()
+    vars_subsystem = VarsSubsystem()
+    regex = vars_subsystem.compile_stats_filters(["alpha", "beta.*"])
+    endpoint = VarsEndpoint(period=Amount(60000, Time.MILLISECONDS), stats_filter=regex)
+    metrics_returned = endpoint.handle_vars_json()
+    assert "zone" in metrics_returned
+    assert "alpha" in metrics_returned
+    request.GET.replace('filtered', None)
 
   def test_filtering_vars_filter_disabled_and_requested(self):
-  	rm = RootMetrics()
-  	zone = NamedGauge('alpha', "wont_be_visible")
-  	alpha = NamedGauge('zone', "smf1")
-  	rm.register(zone)
-  	rm.register(alpha)
+    rm = RootMetrics()
+    zone = NamedGauge('alpha', "wont_be_visible")
+    alpha = NamedGauge('zone', "smf1")
+    rm.register(zone)
+    rm.register(alpha)
 
-	metrics = RootMetrics()
-  	varsSubsystem = VarsSubsystem()
-  	regex = None
-  	endpoint = VarsEndpoint(period = Amount(60000, Time.MILLISECONDS), stats_filter = regex)
-  	request.GET.append('filtered', '1')
-  	metrics_returned = endpoint.handle_vars_json()
-  	assert("zone" in metrics_returned)
-  	assert("alpha" in metrics_returned)
-	request.GET.replace('filtered', None)
+    metrics = RootMetrics()
+    vars_subsystem = VarsSubsystem()
+    regex = None
+    endpoint = VarsEndpoint(period=Amount(60000, Time.MILLISECONDS), stats_filter=regex)
+    request.GET.append('filtered', '1')
+    metrics_returned = endpoint.handle_vars_json()
+    assert "zone" in metrics_returned
+    assert "alpha" in metrics_returned
+    request.GET.replace('filtered', None)
