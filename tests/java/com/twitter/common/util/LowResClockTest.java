41d7c8c325be7bb83885c8ba2a977bb494382f44
John Sirois <john.sirois@gmail.com>
2015-01-04 02:25:21 -0700

    Remove retries and the ThreadDumper.

--- tests/java/com/twitter/common/util/LowResClockTest.java
+++ tests/java/com/twitter/common/util/LowResClockTest.java
@@ -16,27 +16,15 @@
 
 package com.twitter.common.util;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Strings;
-import com.google.common.testing.TearDown;
-import com.google.common.testing.junit4.TearDownTestCase;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import com.twitter.common.testing.junit.rules.Retry;
 import org.easymock.Capture;
 import org.easymock.IAnswer;
-import org.junit.Rule;
 import org.junit.Test;
 
 import com.twitter.common.quantity.Amount;
@@ -54,7 +42,7 @@ import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-public class LowResClockTest extends TearDownTestCase {
+public class LowResClockTest {
 
   /**
    * A FakeClock that overrides the `advance` method to wait for the remote thread to complete
@@ -107,72 +95,8 @@ public class LowResClockTest extends TearDownTestCase {
     }
   }
 
-  static class ThreadDumper {
-    private final Collection<String> lines =
-        Collections.synchronizedCollection(new LinkedList<String>());
-    private final Collection<Thread> threads =
-        Collections.synchronizedCollection(new LinkedHashSet<Thread>());
-
-    private final ScheduledExecutorService scheduler;
-    private final ScheduledFuture<?> dumpRun;
-
-    ThreadDumper() {
-      scheduler = Executors.newScheduledThreadPool(1,
-          new ThreadFactoryBuilder().setNameFormat("Dumper").setDaemon(true).build());
-      Runnable dumper = new Runnable() {
-        @Override public void run() {
-          for (Thread thread : threads) {
-            String header = "Dumping " + thread.getName();
-            lines.add(Strings.repeat("=", header.length()));
-            lines.add(header);
-            for (StackTraceElement element : thread.getStackTrace()) {
-              lines.add("\t" + element);
-            }
-          }
-        }
-      };
-      dumpRun = scheduler.scheduleAtFixedRate(dumper, 500L, 500L, TimeUnit.MILLISECONDS);
-    }
-
-    private boolean stop() {
-      if (!dumpRun.isCancelled()) {
-        dumpRun.cancel(true);
-        scheduler.shutdownNow();
-        return true;
-      }
-      return false;
-    }
-
-    public void discard() {
-      stop();
-    }
-
-    public void dump() {
-      if (stop()) {
-        System.out.println(Joiner.on('\n').join(lines));
-        System.out.flush();
-      }
-    }
-
-    public void add(Thread thread) {
-      threads.add(thread);
-    }
-  }
-
-  @Rule
-  public Retry.Rule rule = new Retry.Rule();
-
-  @Test(timeout = 5000L)
-  @Retry(times = 100000)
+  @Test
   public void testLowResClock() {
-    final ThreadDumper threadDumper = new ThreadDumper();
-    threadDumper.add(Thread.currentThread());
-    addTearDown(new TearDown() {
-      @Override public void tearDown() {
-        threadDumper.dump();
-      }
-    });
-
     final SynchronousQueue<CountDownLatch> queue = new SynchronousQueue<CountDownLatch>();
     final WaitingFakeClock clock = new WaitingFakeClock(queue);
     final long start = clock.nowMillis();
@@ -184,7 +108,7 @@ public class LowResClockTest extends TearDownTestCase {
         eq(TimeUnit.MILLISECONDS));
     expectLastCall().andAnswer(new IAnswer<ScheduledFuture<?>>() {
       public ScheduledFuture<?> answer() {
-        final Thread t = new Thread("Advancer") {
+        final Thread t = new Thread() {
           @Override public void run() {
             long t = start;
             try {
@@ -203,7 +127,6 @@ public class LowResClockTest extends TearDownTestCase {
         };
         t.setDaemon(true);
         t.start();
-        threadDumper.add(t);
         final ScheduledFuture<?> future = createMock(ScheduledFuture.class);
         final AtomicBoolean stopped = new AtomicBoolean(false);
         expect(future.isCancelled()).andAnswer(new IAnswer<Boolean>() {
@@ -245,7 +168,5 @@ public class LowResClockTest extends TearDownTestCase {
     } catch (IllegalStateException e) {
       /* expected */
     }
-
-    threadDumper.discard();
   }
 }
