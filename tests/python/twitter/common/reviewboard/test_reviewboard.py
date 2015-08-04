545fa9157688b148a38c495495ab00f75454938a
Ken Kawamoto <kentaro@twitter.com>
2015-08-03 22:04:30 -0700

    add autospec=True

--- tests/python/twitter/common/reviewboard/test_reviewboard.py
+++ tests/python/twitter/common/reviewboard/test_reviewboard.py
@@ -4,7 +4,7 @@ import mock
 from mock import call
 import pytest
 
-@mock.patch("twitter.common.reviewboard.reviewboard.ReviewBoardServer.http_request")
+@mock.patch.object(ReviewBoardServer, "http_request", autospec=True)
 def test_fetch_review_requests(mock_method):
   mock_method.return_value = '''{
     "stat" : "ok",
@@ -15,12 +15,12 @@ def test_fetch_review_requests(mock_method):
   rbs = ReviewBoardServer('http://example.com', username='user', password='pass1')
   assert len(rbs.fetch_review_requests()) == 2
   assert mock_method.mock_calls == [
-    call("api/review-requests/?start=0&max-results=25",
+    call(rbs, "api/review-requests/?start=0&max-results=25",
          None, None, {'Accept': 'application/json'}, method=None)
   ]
 
 
-@mock.patch("twitter.common.reviewboard.reviewboard.ReviewBoardServer.http_request")
+@mock.patch.object(ReviewBoardServer, "http_request", autospec=True)
 def test_get_reviews(mock_method):
   mock_method.return_value = '''{
     "stat" : "ok",
@@ -32,11 +32,11 @@ def test_get_reviews(mock_method):
   rb_id = 12345
   assert len(rbs.get_reviews(rb_id)) == 2
   assert mock_method.mock_calls == [
-    call("api/review-requests/%d/reviews/?start=0&max-results=25" % rb_id,
+    call(rbs, "api/review-requests/%d/reviews/?start=0&max-results=25" % rb_id,
          None, None, {'Accept': 'application/json'}, method=None)
   ]
 
-@mock.patch("twitter.common.reviewboard.reviewboard.ReviewBoardServer.http_request")
+@mock.patch.object(ReviewBoardServer, "http_request", autospec=True)
 def test_get_replies(mock_method):
   mock_method.return_value = '''{
     "stat" : "ok",
@@ -49,11 +49,11 @@ def test_get_replies(mock_method):
   review_id = 456
   assert len(rbs.get_replies(rb_id, review_id)) == 2
   assert mock_method.mock_calls == [
-    call("api/review-requests/%d/reviews/%d/replies/?start=0&max-results=25" % (rb_id, review_id),
+    call(rbs, "api/review-requests/%d/reviews/%d/replies/?start=0&max-results=25" % (rb_id, review_id),
          None, None, {'Accept': 'application/json'}, method=None)
   ]
 
-@mock.patch("twitter.common.reviewboard.reviewboard.ReviewBoardServer.http_request")
+@mock.patch.object(ReviewBoardServer, "http_request", autospec=True)
 def test_get_changes(mock_method):
   mock_method.return_value = '''{
     "stat" : "ok",
@@ -65,11 +65,11 @@ def test_get_changes(mock_method):
   rb_id = 123
   assert len(rbs.get_changes(rb_id)) == 2
   assert mock_method.mock_calls == [
-    call("api/review-requests/%d/changes/?start=0&max-results=25" % rb_id,
+    call(rbs, "api/review-requests/%d/changes/?start=0&max-results=25" % rb_id,
          None, None, {'Accept': 'application/json'}, method=None)
   ]
 
-@mock.patch("twitter.common.reviewboard.reviewboard.ReviewBoardServer.http_request")
+@mock.patch.object(ReviewBoardServer, "http_request", autospec=True)
 def test_get_diffs(mock_method):
   mock_method.return_value = '''{
     "stat" : "ok",
@@ -81,11 +81,11 @@ def test_get_diffs(mock_method):
   rb_id = 123
   assert len(rbs.get_diffs(rb_id)) == 2
   assert mock_method.mock_calls == [
-    call("api/review-requests/%d/diffs/" % rb_id,
+    call(rbs, "api/review-requests/%d/diffs/" % rb_id,
          None, None, {'Accept': 'application/json'}, method=None)
   ]
 
-@mock.patch("twitter.common.reviewboard.reviewboard.ReviewBoardServer.http_request")
+@mock.patch.object(ReviewBoardServer, "http_request", autospec=True)
 def test_get_files(mock_method):
   mock_method.return_value = '''{
     "stat" : "ok",
@@ -98,11 +98,11 @@ def test_get_files(mock_method):
   revision = 456
   assert len(rbs.get_files(rb_id, revision)) == 2
   assert mock_method.mock_calls == [
-    call("api/review-requests/%d/diffs/%d/files/?start=0&max-results=25" % (rb_id, revision),
+    call(rbs, "api/review-requests/%d/diffs/%d/files/?start=0&max-results=25" % (rb_id, revision),
          None, None, {'Accept': 'application/json'}, method=None)
   ]
 
-@mock.patch("twitter.common.reviewboard.reviewboard.ReviewBoardServer.http_request")
+@mock.patch.object(ReviewBoardServer, "http_request", autospec=True)
 def test_diff_comments(mock_method):
   mock_method.return_value = '''{
     "stat" : "ok",
@@ -116,6 +116,6 @@ def test_diff_comments(mock_method):
   file_id = 789
   assert len(rbs.get_diff_comments(rb_id, revision, file_id)) == 2
   assert mock_method.mock_calls == [
-    call("api/review-requests/%d/diffs/%d/files/%d/diff-comments/?start=0&max-results=25" % (rb_id, revision, file_id),
+    call(rbs, "api/review-requests/%d/diffs/%d/files/%d/diff-comments/?start=0&max-results=25" % (rb_id, revision, file_id),
          None, None, {'Accept': 'application/json'}, method=None)
   ]
